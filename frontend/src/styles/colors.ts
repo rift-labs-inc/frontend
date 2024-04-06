2c765c8fa4ce3516ae2b7220bbe1066c2ea1ae86
Tristan Barrett <bruidbarrett@gmail.com>
2024-04-05 22:20:02 -0700

    ui

--- frontend/src/styles/colors.ts
+++ frontend/src/styles/colors.ts
@@ -1,26 +1,8 @@
 export default {
-  primaryBlue: "#307BF4",
-  secondaryBlue: "#3664AF",
-  tertiaryBlue: "#213f72",
-  lightBlue: "#595C61",
-
-  offWhite: "#FAF4E8",
-  offBlack: "#0e0e0e",
-
-  textGray: "#A8A8A8",
-
-  backgroundGray: "#1b1b1b",
-  editorGray: "#121212",
-  appbarGray: "#282828",
-  innerGray: "#343434",
-
-  gray: "#BFBFBF",
-  lightGray: "#808080",
-  darkGray: "#4A4A4A",
-  newGray: "#9F9F9F",
-  darkerGray: "#2c3035",
-
-  redFaded: "#AD2B2B",
-  redLight: "#fa4142",
-  red: "red",
+  RiftOrange: "##FF8F28",
+  RiftBlue: "#394AFF",
+  White: "#ffffff",
+  offWhite: "#eeeeee",
+  textGray: "#a5a5a5",
+  offBlack: "#212121",
 };
